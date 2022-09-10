91e68ccff8400c36133828e622f03ff012842f84
RereGit01 <rereloluwaobadeyi555@gmail.com>
2022-09-09 21:44:14 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
+
 /**
-*main - Entry point
-*Description: Get a random number and check its last digit, compare it with 5
-*Return: 0
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
 */
 int main(void)
 {
-char c = 'a';
-while (c <= 'z')
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {	
-if (c != 'e' && c != 'q')
-putchar(c);
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
 }
-c++
 }
-putchar('\n');
+
+putchar(10); /* this is an ascii code for new line*/
+
 return (0);
 }
